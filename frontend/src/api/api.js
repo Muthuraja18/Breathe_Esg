17da777bebe82eb6ce2a175d42ab0bff98e89ac9
Muthuraja18 <muthurajlingam788@gmail.com>
2026-05-29 00:27:48 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,56 +1,40 @@
 import axios from "axios";
 
-
-// ==========================
-// BASE API
-// ==========================
 const API = axios.create({
-
-    baseURL:
-        "https://breathe-esg1-59hl.onrender.com/api/emissions/"
-
+    baseURL: "https://breathe-esg1-59hl.onrender.com/api/emissions/",
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
-
 
 // ==========================
 // GET EMISSIONS
-// USER-WISE FILTER
 // ==========================
 export const getEmissions = (email) =>
-
     API.get(`?email=${email}`);
-
 
 // ==========================
 // APPROVED RECORDS
 // ==========================
 export const getApprovedRecords = (email) =>
-
     API.get(`approved/?email=${email}`);
-
 
 // ==========================
 // SUSPICIOUS RECORDS
 // ==========================
 export const getSuspicious = (email) =>
-
     API.get(`suspicious/?email=${email}`);
-
 
 // ==========================
 // APPROVE RECORD
 // ==========================
 export const approveRecord = (id) =>
-
     API.post(`approve/${id}/`);
-
 
 // ==========================
 // DASHBOARD STATS
 // ==========================
 export const getDashboardStats = (email) =>
-
     API.get(`dashboard/?email=${email}`);
-
 
 export default API;
